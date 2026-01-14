2358a290192ccdeda0a56cb12f13ee9decdda3d2
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2026-01-14 10:32:18 +0100

    Enhance JWT authentication by implementing JwtConverter for role extraction and principal name resolution. Update SecurityConfig to utilize the new converter and configure role-based access for the /students/** endpoint. Modify application.properties to define principal attribute and resource ID for JWT processing.

--- gateway/src/main/java/com/skistation/gateway/securityConfig/SecurityConfig.java
+++ gateway/src/main/java/com/skistation/gateway/securityConfig/SecurityConfig.java
@@ -2,9 +2,9 @@ package com.skistation.gateway.securityConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
@@ -12,14 +12,27 @@ import org.springframework.security.web.server.context.NoOpServerSecurityContext
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    private final JwtConverter jwtConverter;
+
+    public SecurityConfig(JwtConverter jwtConverter) {
+        this.jwtConverter = jwtConverter;
+    }
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth
                         .pathMatchers("/actuator/*").permitAll()
+                        .pathMatchers("/students/**").hasRole("ADMIN")
                         .anyExchange().authenticated())
+                // Configure JWT-based OAuth2 Resource Server
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults()))
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(
+                                        new ReactiveJwtAuthenticationConverterAdapter(jwtConverter)
+                                ) // Converts JWT claims to Spring Security authorities
+                        )
+                )
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .build();
     }
